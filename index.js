7da4212a7f727fc14cf39538bf8fbd1a02b1c4e6
Raminta <1989sliekiukasss@gmail.com>
2024-08-22 14:51:51 +0300

    new pages

--- index.js
+++ index.js
@@ -7,16 +7,46 @@ const port = 3000;
 
 // req ir res - trumpiniai 
 // nepamirsti prisirasyti return 
+// '/' - nurodome kelia
 app.get('/', (request, response) => {
-    return response.send('Labas rytas, Lietuva');
+    return response.send('Home page');
 })
 
 app.get('/about', (req, res) => {
-    return res.send('Nori sužinoti apie šį projektą?');
+    return res.send('About page');
 })
 
+app.get('/services', (req, res) => {
+    return res.send('Services page');
+})
+
+app.get('/services/design', (req, res) => {
+    return res.send('Services page: design');
+})
+
+app.get('/services/ux', (req, res) => {
+    return res.send('Services page: UX');
+})
+
+app.get('/services/coding', (req, res) => {
+    return res.send('Services page: Programming');
+})
+
+app.get('/services/hacking', (req, res) => {
+    return res.send('Services page: Hacking');
+})
+
+app.get('/services/*', (req, res) => {
+    return res.send('Services page: such service is not recognized...');
+})
+
+app.get('/services/team', (req, res) => {
+    return res.send('Team page: Team page');
+})
+
+// * turi buti paskutine, nes  kitu etveju jis butu pirmas, ir grazintas tik jorezultatas esantis puslapyje
 app.get('*', (req, res) => {
-    return res.send('nooo');
+    return res.send('Ups... 404 page 🦟');
 })
 
 app.listen(port, () => {
