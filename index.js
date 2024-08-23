a83c76807094b38ebdb3801963c74db095fa253e
Raminta <1989sliekiukasss@gmail.com>
2024-08-23 14:56:51 +0300

    router

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 // require - senasis node. atsinaujiname su import
 import express from 'express';
-import { servicesData } from './data/servicesData.js';
-import { members } from './data/members.js';
 import { students } from './data/students.js';
+import { servicesRouter } from './router/servicesRouter.js';
 
 //inicijuoja app
 const app = express();
@@ -19,118 +18,11 @@ app.get('/about', (req, res) => {
     return res.send('About page');
 })
 
-// paslaugu sarasas
-app.get('/services', (req, res) => {
-    return res.send('Services page');
-})
-
-// Like 4 routai (uzkomentuoti) nebeveiks, nes parametrizuotas reikalas esantis :serviceName, pagauna visus kitus atvejus.
-// Jei norime kazka individualiai daryti su zemiau esanciomis services paslaugomis tai iskelti ji po Servisec page.
-
-app.get('/services/:serviceName', (req, res) => {
-    if (servicesData.includes(req.params.serviceName)) {
-        return res.send(`About "${req.params.serviceName}" service...`)
-    }
-    return res.send('Services page: such service is not recognized...');
-})
-
-// darome is uzrasu praktiskai
-app.get('/services/:serviceName/members', (req, res) => {
-    if (servicesData.includes(req.params.serviceName)) {
-        return res.send(`Paslaugos "${req.params.serviceName}" nariu saras...`)
-    }
-    return res.send('Services page: such service is not recognized...');
-})
-
-// member name uzrasymo pvz:
-// app.get('/services/:serviceName/members/:memberName', (req, res) => {
-//     const serviceName = req.params.serviceName;
-//     const memberName = req.params.memberName;
-
-//     const response1 = `Paslaugos "${serviceName}" nario "${memberName}" informacija...`;
-//     const response2 = `Paslaugoje "${serviceName}" nario "${memberName}" nepavyko rastis...`;
-//     const response3 = 'Services page: such service is not recognized...';
-
-//     if (servicesData.includes(serviceName)) {
-//         if (members.includes(memberName)) {
-//             return res.send(response1)
-//         }
-//         return res.send(response2)
-//     }
-//     return res.send(response3);
-// })
-
-// kitas pvz, naudojame neigini:
-
-// app.get('/services/:serviceName/members/:memberName', (req, res) => {
-//     const serviceName = req.params.serviceName;
-//     const memberName = req.params.memberName;
-
-//     const response1 = `Paslaugos "${serviceName}" nario "${memberName}" informacija...`;
-//     const response2 = `Paslaugoje "${serviceName}" nario "${memberName}" nepavyko rastis...`;
-//     const response3 = 'Services page: such service is not recognized...';
-
-//     if (!servicesData.includes(serviceName)) {
-//         return res.send(response3);
-//     }
-//     if (!members.includes(memberName)) {
-//         return res.send(response2)
-//     }
-
-//     return res.send(response1)
-// });
-
-app.get('/services/:serviceName/members/:memberName', (req, res) => {
-    // destrukturizavimas ivyksta
-    const { serviceName, memberName } = req.params;
-
-    if (!servicesData.includes(serviceName)) {
-        return res.send('Services page: such service is not recognized...');
-    }
-
-    if (!members.includes(memberName)) {
-        return res.send(`Paslaugoje "${serviceName}" nario "${memberName}" nepavyko rastis...`)
-    }
-
-    return res.send(`Paslaugos "${serviceName}" nario "${memberName}" informacija...`)
-});
+app.use('/services', servicesRouter);
+app.use('/team', teamRouter);
 
 
-// app.get('/services/design', (req, res) => {
-//     return res.send('Services page: design');
-// })
 
-// app.get('/services/ux', (req, res) => {
-//     return res.send('Services page: UX');
-// })
-
-// app.get('/services/coding', (req, res) => {
-//     return res.send('Services page: Programming');
-// })
-
-// app.get('/services/hacking', (req, res) => {
-//     return res.send('Services page: Hacking');
-// })
-
-// app.get('/services/*', (req, res) => {
-//     return res.send('Services page: such service is not recognized...');
-// })
-
-app.get('/team', (req, res) => {
-    return res.send('Team page');
-})
-
-// app.get('/team/chuck', (req, res) => {
-//     return res.send('Team member page: Chuck');
-// })
-
-app.get('/team/:name', (req, res) => {
-    const members = ['Chuck', 'lolo', 'prime']
-    if (members.includes(req.params.name)) {
-        return res.send(`Team member "${req.params.name}" all info about members`);
-    }
-    return res.send(`Team member "${req.params.name}" page not found`);
-})
 
 app.get('/students', (req, res) => {
     // const names = [];
@@ -173,6 +65,35 @@ app.get('/students/:name', (req, res) => {
         return res.send(`Studento, vardu ${req.params.name} nera...`)
     }
 })
+
+app.get('/books', (req, res) => {
+    return res.send('GET: books')
+})
+
+app.post('/books', (req, res) => {
+    return res.send('POST: books')
+})
+app.put('/books', (req, res) => {
+    return res.send('PUT: books')
+})
+app.delete('/books', (req, res) => {
+    return res.send('DELETE: books')
+})
+
+app.route('/phones')
+    .get((req, res) => {
+        return res.send('GET: phones');
+    })
+    .post((req, res) => {
+        return res.send('POST: phones');
+    })
+    .put((req, res) => {
+        return res.send('PUT: phones');
+    })
+    .delete((req, res) => {
+        return res.send('DELETE: phones');
+    })
+
 
 // app.get('/sal/*-sale', (req, res) => {
 //     return res.send('Midseason Sales page');
---
+++ router/teamRouter.js
@@ -0,0 +1,19 @@
+import express from 'express';
+
+export const teamRouter = express.Router();
+
+teamRouter.get('/team', (req, res) => {
+    return res.send('Team page');
+})
+
+// app.get('/team/chuck', (req, res) => {
+//     return res.send('Team member page: Chuck');
+// })
+
+teamRouter.get('/:name', (req, res) => {
+    const members = ['Chuck', 'lolo', 'prime']
+    if (members.includes(req.params.name)) {
+        return res.send(`Team member "${req.params.name}" all info about members`);
+    }
+    return res.send(`Team member "${req.params.name}" page not found`);
+})
