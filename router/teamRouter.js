23989cfa0063d3a355e6d7d83a6938f6f0bafbe0
Raminta <1989sliekiukasss@gmail.com>
2024-08-26 09:26:45 +0300

    teamRouter fixed

--- router/teamRouter.js
+++ router/teamRouter.js
@@ -2,7 +2,7 @@ import express from 'express';
 
 export const teamRouter = express.Router();
 
-teamRouter.get('/team', (req, res) => {
+teamRouter.get('/', (req, res) => {
     return res.send('Team page');
 })
 
@@ -11,9 +11,9 @@ teamRouter.get('/team', (req, res) => {
 // })
 
 teamRouter.get('/:name', (req, res) => {
-    const members = ['Chuck', 'lolo', 'prime']
+    const members = ['Chuck', 'lolo', 'prime'];
     if (members.includes(req.params.name)) {
-        return res.send(`Team member "${req.params.name}" all info about members`);
+        return res.send(`Team member: "${req.params.name}" all info about this members`);
     }
     return res.send(`Team member "${req.params.name}" page not found`);
 })
