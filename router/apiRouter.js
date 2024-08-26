bab4b4e5f9947920c0cefa5c3e9cbe388d4ff8a6
Raminta <1989sliekiukasss@gmail.com>
2024-08-26 13:16:49 +0300

    apiRouter

--- router/apiRouter.js
+++ router/apiRouter.js
@@ -1,5 +1,45 @@
+import express from 'express';
+
+export const apiRouter = express.Router();
+
+// GET: /api/
+apiRouter.get('/', (req, res) => {
+    const data = {
+        state: 'error',
+        message: 'Nurodyk konkretu API endpoint\'a',
+    };
+    // return res.json(data);
+    return res.send(JSON.stringify(data));
+});
+
+apiRouter.get('/my-marks', (req, res) => {
+    const marks = [];
+    return res.json(marks);
+});
+
+apiRouter.get('/my-marks', (req, res) => {
+    const marks = [10, 2, 8, 4, 6];
+    return res.json(marks);
+});
+
 // API - aplication programming interface
 // RestAPI
 // statefull API
 // stateless API
+
+// GET: /api//marks
+// error: nes nenurodei studento vardo?
+
+// GET: /api/marks/:studentName
+// GET: /api/marks/Jonas
+// []
+// [10, 2]
+
+// POST: /api/marks -> 10
+// error, kurima studentui?
+
+// POST: /api/marks -> {mark: 10, name: 'Jonas'}
+// ok
+
+
 
